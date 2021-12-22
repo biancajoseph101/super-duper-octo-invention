be8a094f806aa5285ac7c4b46fd98413e528bfbd
biancajoseph101 <biancajoseph101@gmail.com>
2021-12-22 17:00:12 -0600

    added map title

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -4,6 +4,7 @@ import '../styles/Map.css';
 const Map = () => {
   return (
     <div className="map">
+      <h1>Map</h1>
       <img
         className="map"
         src="https://images.unsplash.com/photo-1515861461225-1488dfdaf0a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
